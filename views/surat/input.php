b1c33e741516cd915a93ff798940c1c8649a9cb1
shinataputra <71615861+shinataputra@users.noreply.github.com>
2025-07-27 08:29:01 +0700

    update : tambah fav icon

--- views/surat/input.php
+++ views/surat/input.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Form Surat Ijin Siswa</title>
+    <link rel="icon" type="image/png" href="assets/img/iconsmexa.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
