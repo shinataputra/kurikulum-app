2497cc4fad0429152b3152bd61e3026fc46d509b
shinataputra <71615861+shinataputra@users.noreply.github.com>
2025-07-27 10:44:18 +0700

    feat : cetak dan filter pada admin method rekapfilter() dan cetak()

--- app/controller/AdminController.php
+++ app/controller/AdminController.php
@@ -1,5 +1,8 @@
 <?php
 require_once(__DIR__ . '/../Model/SuratIjin.php');
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Dompdf\Dompdf;
 
 class AdminController
 {
@@ -9,5 +12,35 @@ class AdminController
         $suratIjin = new SuratIjin();
         $data = $suratIjin->all();
         include(__DIR__ . '/../../views/admin/rekap.php');
+    }
+
+    public function rekapfilter()
+    {
+        $dari = $_GET['dari'] ?? null;
+        $sampai = $_GET['sampai'] ?? null;
+
+        $suratIjin = new SuratIjin();
+        $data = $suratIjin->getByDateRange($dari, $sampai);
+
+        include(__DIR__ . '/../../views/admin/rekap.php');
+    }
+
+    public function cetak()
+    {
+        $dari = $_GET['dari'] ?? null;
+        $sampai = $_GET['sampai'] ?? null;
+
+        $suratIjin = new SuratIjin();
+        $data = $suratIjin->getByDateRange($dari, $sampai);
+
+        ob_start();
+        include(__DIR__ . '/../../views/admin/cetak_rekap.php'); // file HTML khusus cetak
+        $html = ob_get_clean();
+
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream('rekap_surat_ijin.pdf', ['Attachment' => false]);
     }
 }
